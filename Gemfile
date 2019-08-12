80b2a9195a50a826341162b3a73e37ecc3b2d80f
kierepkae <kierepkaedyta@gmail.com>
2019-08-12 14:53:16 +0100

    setting up the project

--- Gemfile
+++ Gemfile
@@ -4,3 +4,6 @@ gem 'sinatra', '2.0.5'
 gem 'rspec', '3.8.0'
 gem 'selenium-webdriver', '3.142.3'
 gem 'capybara', '3.28.0'
+gem 'rubocop', '0.74.0'
+gem 'simplecov', require: false, group: :test
+gem 'rspec-sinatra', '0.1.2'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,16 @@
 
+# require 'simplecov'
+# require 'simplecov-console'
+
+# SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+#   SimpleCov::Formatter::Console,
+#   # Want a nice code coverage website? Uncomment this next line!
+#   # SimpleCov::Formatter::HTMLFormatter
+# ])
+# SimpleCov.start
+
+require 'simplecov'
+SimpleCov.start
 ENV['RACK_ENV'] = 'test'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
@@ -6,6 +18,7 @@ require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
 Capybara.app = BookmarkManager
+
 
 
 
