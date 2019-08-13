55e49d973cf76a844386fd10e473de05aab062f0
kierepkae <kierepkaedyta@gmail.com>
2019-08-13 12:03:36 +0100

    set up setup_test_database to clean and setup database before each test - step 9

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,12 @@
-
+require  'setup_test_database'
 ENV['ENVIRONMENT'] = 'test'
-# require 'simplecov'
-# require 'simplecov-console'
 
-# SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-#   SimpleCov::Formatter::Console,
-#   # Want a nice code coverage website? Uncomment this next line!
-#   # SimpleCov::Formatter::HTMLFormatter
-# ])
-# SimpleCov.start
+RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+end
+
 
 require 'simplecov'
 SimpleCov.start
